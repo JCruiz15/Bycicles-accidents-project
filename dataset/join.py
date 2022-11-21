55ce8309c536d8bb77ed7d6c038ca4db009da808
JCruiz15 <juancaruru@uma.es>
2022-11-21 11:45:24 +0100

    Added the dataset csv and the first steps of XML Schema

---
+++ dataset/join.py
@@ -0,0 +1,18 @@
+# Python script that obtains both csv and joins them by the Accident_Index. Then it saves the complete results in one file and creates a file with around 100 samples
+
+import math
+import pandas as pd
+
+accidents = pd.read_csv("Accidents.csv")
+bikers = pd.read_csv("Bikers.csv")
+bikers = bikers.rename({'Gender': 'biker_gender', 'Severity': 'biker_severity_injury', 'Age_Grp': 'biker_age_group'}, axis=1)  
+
+result = pd.merge(accidents, bikers, on="Accident_Index")
+
+result.to_csv("final_dataset_full.csv", header=True, sep=";", decimal=".", index=False)
+
+result_100 = result[round(len(result)*0.50187):round(len(result)*0.502)]
+
+print(len(result_100))
+
+result_100.to_csv("final_dataset_100samples.csv", header=True, sep=";", decimal=".", index=False)
